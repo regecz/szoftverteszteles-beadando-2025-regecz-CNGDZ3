f1c7f57c0e06c39aa7e6e93a3f7e31b13e21fa42
Regecz <regecz.mark@gmail.com>
2025-11-04 14:05:57 +0100

    Initial commit

---
+++ src/main/java/uni/miskolc/java/szoftvertesztelesbeadando2025regeczcngdz3/SzoftvertesztelesBeadando2025RegeczCngdz3Application.java
@@ -0,0 +1,13 @@
+package uni.miskolc.java.szoftvertesztelesbeadando2025regeczcngdz3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SzoftvertesztelesBeadando2025RegeczCngdz3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SzoftvertesztelesBeadando2025RegeczCngdz3Application.class, args);
+	}
+
+}
---
+++ src/test/java/uni/miskolc/java/szoftvertesztelesbeadando2025regeczcngdz3/SzoftvertesztelesBeadando2025RegeczCngdz3ApplicationTests.java
@@ -0,0 +1,13 @@
+package uni.miskolc.java.szoftvertesztelesbeadando2025regeczcngdz3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SzoftvertesztelesBeadando2025RegeczCngdz3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
